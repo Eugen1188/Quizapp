ccd782890f53413f4a09fccaa52e58cef649054a
Eugen Ferchow <Eugen.Ferchow@gmx.de>
2022-02-25 04:19:07 +0100

    update

--- script.js
+++ script.js
@@ -70,5 +70,13 @@ function showQuestion(){
     let question = questions[currentQuestion];
 
     document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection){
+
 
 }
